51ccdddf6d6921af66cf2c770a72623deb406951
ernarkunanbaev70-droid <ernarkunanbaev70@gmail.com>
2026-01-06 10:39:29 +0500

    Update ClothingItem.java

--- ClothingItem.java
+++ ClothingItem.java
@@ -1,26 +1,45 @@
 public class ClothingItem {
-    protected int id;
     protected String name;
     protected String brand;
     protected double price;
 
-    public ClothingItem(int id, String name, String brand, double price) {
-        this.id = id;
+    public ClothingItem(String name, String brand, double price) {
+        setName(name);
+        setBrand(brand);
+        setPrice(price);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        if (brand == null || brand.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.brand = brand;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException();
+        }
         this.price = price;
     }
 
-    public void show() {
-        System.out.println(name + " | " + brand + " | " + price + " KZT");
+    public String type() {
+        return "Clothing Item";
     }
 
-    public String type() {
-        return "Clothing";
+    public void show() {
+        System.out.println(type() + ": " + name + ", " + brand + ", $" + price);
     }
 
     @Override
     public String toString() {
-        return "[" + type() + "] " + name + ", " + brand + ", " + price + " KZT";
+        return type() + ": " + name + ", " + brand + ", $" + price;
     }
 }
