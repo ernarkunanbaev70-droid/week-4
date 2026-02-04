1334524af491fdba7cb7ff04bc1c95b7b48e2b1e
ernarkunanbaev70-droid <ernarkunanbaev70@gmail.com>
2026-02-04 20:00:57 +0500

    Update MenuManager.java

--- src/menu/MenuManager.java
+++ src/menu/MenuManager.java
@@ -21,6 +21,9 @@ public class MenuManager implements Menu {
             4. View ALL products (sorted by price)
             5. View ALL products (no sorting)
             6. View CLOTHING only
+            7. Search by name
+            8. Search by price range
+            9. Search by min price
             0. Exit
             """);
     }
@@ -38,11 +41,13 @@ public class MenuManager implements Menu {
                 case 4 -> viewAllProducts();
                 case 5 -> viewAllProductsUnsorted();
                 case 6 -> viewClothingItems();
+                case 7 -> searchByName();
+                case 8 -> searchByPriceRange();
+                case 9 -> searchByMinPrice();
                 case 0 -> System.exit(0);
             }
         }
     }
-
 
     @Override
     public void viewAllProductsUnsorted() {
@@ -65,13 +70,13 @@ public class MenuManager implements Menu {
         }
     }
 
-
-
     private void add() {
         System.out.print("Name: ");
         String n = sc.nextLine();
+
         System.out.print("Price: ");
         double p = Double.parseDouble(sc.nextLine());
+
         System.out.print("Size: ");
         String s = sc.nextLine();
 
@@ -81,18 +86,61 @@ public class MenuManager implements Menu {
     private void update() {
         System.out.print("ID: ");
         int id = Integer.parseInt(sc.nextLine());
+
         System.out.print("New price: ");
         double price = Double.parseDouble(sc.nextLine());
-        dao.update(id, price);
+
+        if (dao.update(id, price)) {
+            System.out.println("Updated");
+        } else {
+            System.out.println("Product not found");
+        }
     }
 
     private void delete() {
         System.out.print("ID: ");
-        dao.delete(Integer.parseInt(sc.nextLine()));
+        int id = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Are you sure? (yes/no): ");
+        String confirm = sc.nextLine();
+
+        if (confirm.equalsIgnoreCase("yes")) {
+            if (dao.delete(id)) {
+                System.out.println("Deleted");
+            } else {
+                System.out.println("Product not found");
+            }
+        } else {
+            System.out.println("Cancelled");
+        }
     }
 
-    private void view() {
-        for (Product p : dao.getAllSortedByPrice()) {
+    private void searchByName() {
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        for (Product p : dao.searchByName(name)) {
+            System.out.println(p.info());
+        }
+    }
+
+    private void searchByPriceRange() {
+        System.out.print("Min price: ");
+        double min = Double.parseDouble(sc.nextLine());
+
+        System.out.print("Max price: ");
+        double max = Double.parseDouble(sc.nextLine());
+
+        for (Product p : dao.searchByPriceRange(min, max)) {
+            System.out.println(p.info());
+        }
+    }
+
+    private void searchByMinPrice() {
+        System.out.print("Min price: ");
+        double min = Double.parseDouble(sc.nextLine());
+
+        for (Product p : dao.searchByMinPrice(min)) {
             System.out.println(p.info());
         }
     }
